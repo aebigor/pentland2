3d7d7abdb11fafdd2657dfdc0de595d0ce32fc60
aebigor <santris162@gmail.com>
2024-03-14 20:48:40 -0500

    Creación del app service php y conexión a base de datos

--- asset/db/consultas.sql
+++ asset/db/consultas.sql
@@ -4,6 +4,10 @@ INSERT INTO ROLES VALUES
 (null, 'customer'),
 (null, 'seller');
 
+INSERT INTO USUARIO VALUES 
+(null, 'Pepito', 'Perez', 'pepito@perez.com', '12345', 'Vendedor');
+
+
 INSERT INTO productos VALUES 
 (null, 'jabón', 'Jabón azultol', 8000, 5, 'imagen del producto'),
 (null, 'ringo', 'comida para adulto', 18000, 15, 'imagen del producto');
--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
-require_once "models/users/db1.php";
+// require_once "models/users/db1.php";
 require_once "models/users/db.php";
-    if (!isset($_REQUEST['c'])) {
-        require_once "controller/Menu.php";
-        $controller = new menu;
-        $controller->main();
-    } else {
-        $controller = $_REQUEST['c'];
-        require_once "controller/" . $controller . ".php";
-        $controller = new $controller;
-        $action = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'main';
-        if (method_exists($controller, $action)) {
-            // Llamar a la acción solicitada en el controlador
-            $controller->$action();
-        } else {
-            // Acción no válida, manejar el error como desees
-            echo "Error: Acción no válida.";
-        }
-    }
+$prueba = DataBase::connection();
+
+
+    // if (!isset($_REQUEST['c'])) {
+    //     require_once "controller/Menu.php";
+    //     $controller = new menu;
+    //     $controller->main();
+    // } else {
+    //     $controller = $_REQUEST['c'];
+    //     require_once "controller/" . $controller . ".php";
+    //     $controller = new $controller;
+    //     $action = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'main';
+    //     if (method_exists($controller, $action)) {
+    //         // Llamar a la acción solicitada en el controlador
+    //         $controller->$action();
+    //     } else {
+    //         // Acción no válida, manejar el error como desees
+    //         echo "Error: Acción no válida.";
+    //     }
+    // }
     
 ?>
