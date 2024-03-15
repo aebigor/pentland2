a7e963b24acf573ff9e0dd293ecb18da79b0c0f5
aebigor <santris162@gmail.com>
2024-03-14 20:29:40 -0500

    Creación del app service php y conexión a base de datos

--- asset/db/consultas.sql
+++ asset/db/consultas.sql
@@ -4,4 +4,9 @@ INSERT INTO ROLES VALUES
 (null, 'customer'),
 (null, 'seller');
 
+INSERT INTO productos VALUES 
+(null, 'jabón', 'Jabón azultol', 8000, 5, 'imagen del producto'),
+(null, 'ringo', 'comida para adulto', 18000, 15, 'imagen del producto');
+
 SELECT * FROM ROLES;
+SELECT * FROM PRODUCTOS;
