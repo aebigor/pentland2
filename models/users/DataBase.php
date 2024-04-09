6ffa85622185fd38289fafbdfaacf3c1d2877cba
aebigor <santicris162gmail.com>
2024-04-09 16:20:36 -0500

    version demo

--- models/users/DataBase.php
+++ models/users/DataBase.php
@@ -2,30 +2,30 @@
     
     class DataBase{       
     
-        //  public static function connection(){            
-        //     $hostname = "petland.mysql.database.azure.com";
-        //     $port = "3306";
-        //     $database = "pentland";
-        //     $username = "petland";
-        //     $password = "Santy1314";
-        //     $options = array(
-        //         PDO::MYSQL_ATTR_SSL_CA => 'asset/db/DigiCertGlobalRootCA.crt.pem'
-        //     );
-		// 	$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
-		// 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		// 	return $pdo;
-		// }} 
-
-        public static function connection(){            
-            $hostname = "localhost";
+         public static function connection(){            
+            $hostname = "petland.mysql.database.azure.com";
             $port = "3306";
             $database = "pentland";
-            $username = "root";
-            $password = "";
-			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+            $username = "petland";
+            $password = "Santy1314";
+            $options = array(
+                PDO::MYSQL_ATTR_SSL_CA => 'asset/db/DigiCertGlobalRootCA.crt.pem'
+            );
+			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
-		}
+		}} 
+
+    //     public static function connection(){            
+    //         $hostname = "localhost";
+    //         $port = "3306";
+    //         $database = "pentland";
+    //         $username = "root";
+    //         $password = "";
+	// 		$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+	// 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// 		return $pdo;
+	// 	}
 	
-	}
+	// }
 ?>
